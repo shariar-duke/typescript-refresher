e326b60568892f557df9a52a0d7c80c146279028
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-29 16:43:17 +0600

    smart way of declaring array

--- src/hello.ts
+++ src/hello.ts
@@ -1,8 +1,1 @@
-let a: any = 34;
-
-// Convert 'a' to a string explicitly
-let len: number = a.toString().indexOf("3");
-
-console.log(len); // Outputs: 1
-
-// evbe kno kisur upr string method apply korar age take stirng a convert kore nile typeError gula theke bacha jabe
+let names: Array<String> = ["shariar", "mahamdu", "duke"];
