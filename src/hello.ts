704a5071f51ac39bda1cae6240153d0dd5d04084
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-29 16:42:13 +0600

    simple array sintax in typeScript

--- src/hello.ts
+++ src/hello.ts
@@ -4,3 +4,5 @@ let a: any = 34;
 let len: number = a.toString().indexOf("3");
 
 console.log(len); // Outputs: 1
+
+// evbe kno kisur upr string method apply korar age take stirng a convert kore nile typeError gula theke bacha jabe
