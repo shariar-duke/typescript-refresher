f3006928a83a575bb48e353db7d071d0eb5a660e
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-29 17:06:41 +0600

    Enum example

--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,11 @@
-let houseTuples: [number, string, number] = [2, "house", 2];
+enum Status {
+  NOT_STARTED = "stop",
+  IN_PROGRESS = "progress",
+  COMPLETED = "done",
+}
+
+let statuss: Status = Status.COMPLETED;
+
+console.log(statuss);
+
+// ekhne statuss er type Status . tar mane er value ta hote hbe Status objec ter jno ekta value tai eta Status.COMPLETED diyeche
