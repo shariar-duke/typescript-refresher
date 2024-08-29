19bfefbab0950f15591b1e99e3e976162cc624f4
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-29 16:57:53 +0600

    multidynmension array

--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,6 @@
-let names: Array<String> = ["shariar", "mahamdu", "duke"];
+let multidimArr: (number[] | string[])[] = [
+  [1, 2, 3],
+  ["shariar", "mahmud", "duke"],
+];
+
+// ekhen bujano hsce j ekta ekta array er modhe ekta number er array thkbe or ekta stirng er array thakbe
