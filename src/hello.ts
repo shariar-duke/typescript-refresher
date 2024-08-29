178e3c5afcb4b93d0545ca7016904f6ef93db288
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-29 19:43:43 +0600

    creating class from interface

--- src/hello.ts
+++ src/hello.ts
@@ -7,3 +7,17 @@ const rahim: Customer = {
   name: "Rahim",
   address: "Hong Kong",
 };
+
+class GoldenChase implements Customer {
+  name: string;
+  address: string;
+
+  constructor(name: string, address: string) {
+    this.name = name;
+    this.address = address;
+  }
+}
+
+const abcd = new GoldenChase("ABCD", "Dhaka");
+
+console.log(abcd.name);
