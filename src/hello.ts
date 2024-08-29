bc4e35718fe08ba31339b3d2c140176f9b7b24b2
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-29 18:11:41 +0600

    nested type collection

--- src/hello.ts
+++ src/hello.ts
@@ -1,13 +1,42 @@
 type Employe = {
   id: number;
   name: string;
-  email: string;
+  email?: string;
   salary: number;
 };
 
-const employee: Employe = {
-  id: 1,
-  name: "Shariar",
-  email: "shariarmahmud225@gmail.com",
-  salary: 2000,
+type Deparment = {
+  name: string;
+  employees: Employe[];
+};
+
+// ekhn onk gula employer ekta array banabo jar type dbo Employe
+
+const employeeCollection: Employe[] = [
+  {
+    id: 1,
+    name: "Shariar",
+    // email:"shariarmahmud225@gmail.com",
+    salary: 2000,
+  },
+
+  {
+    id: 2,
+    name: "Shariar",
+    email: "shariarmahmud225@gmail.com",
+    salary: 2000,
+  },
+  {
+    id: 3,
+    name: "Shariar",
+    email: "shariarmahmud225@gmail.com",
+    salary: 2000,
+  },
+];
+
+// ekhn ekta deparment banabo jar modhe dept er name thakbe and employee er ekta collection o thakbe
+
+const newDepartment: Deparment = {
+  name: "ECE",
+  employees: employeeCollection,
 };
