364574f12600351f1b61a59fedce574b4e820aca
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-30 10:36:44 +0600

    basic of generic

--- src/hello.ts
+++ src/hello.ts
@@ -1,23 +1,12 @@
-interface Customer {
-  name: string;
-  address: string;
+function greetings<T>(name: T) {
+  console.log(`Hello ${name}. Type of ${name} is ${typeof name} `);
 }
 
-const rahim: Customer = {
-  name: "Rahim",
-  address: "Hong Kong",
-};
+greetings({
+  name: "Shariar",
+  age: 29,
+});
 
-class GoldenChase implements Customer {
-  name: string;
-  address: string;
+// (name:T) ekne name er type ta T dise . And T ta holo ekta generic type . tar mane ami call korar somoy j type a dibo setai se accept krote parbe
 
-  constructor(name: string, address: string) {
-    this.name = name;
-    this.address = address;
-  }
-}
-
-const abcd = new GoldenChase("ABCD", "Dhaka");
-
-console.log(abcd.name);
+// ekhn etar type sudhu T dile vbbe pura file a T name hoyto kno type define kora ase. but pabe na . Tai generic type declare korar age seta define korar way hosche  function greetings<T>(name: T) . evbe  (name:T) etar age <T>
